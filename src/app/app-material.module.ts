1f6e5372e41fec97687e2e42dd579bcb6fcb8a96
MathieuCoding <polletmathieu.pro@gmail.com>
2023-11-13 13:31:20 +0100

    first commit

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  exports: [
+    // BrowserAnimationsModule,
+    // LayoutModule,
+    // MatToolbarModule,
+    // MatButtonModule,
+    // MatCheckboxModule,
+    // MatSidenavModule,
+    // MatIconModule,
+    // MatListModule,
+    // MatTableModule,
+    // MatPaginatorModule,
+  ]
+})
+export class AppMaterialModule { }
